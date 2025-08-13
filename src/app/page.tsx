fa54a1099d2356b4b9de9333225d59b094e6ded2
ManojGollamandala <manojgollamandala1@gmail.com>
2025-08-13 17:32:57 +0530

    Initial project commit with Products.tsx fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 "use client"
 
 
-import React, { useMemo, useState } from "react";
+import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import Features from "../components/Features";
-  import Products from "../components/Products";
+import Products from "../components/Products";
 import About from "../components/About";
 import Contact from "../components/Contact";
 import Enquiry from "../components/Enquiry";
@@ -21,7 +20,7 @@ export default function App() {
     <div className="min-h-screen w-full bg-gray-50 text-gray-900">
       <Navbar whatsappLink={whatsappLink} />
       <Hero whatsappLink={whatsappLink} />
-      <Features />
+      
       <Products whatsappLink={whatsappLink} />
       <About />
       <Contact whatsappLink={whatsappLink} />
